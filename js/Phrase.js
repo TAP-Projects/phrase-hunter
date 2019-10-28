081b1ad93948caef4152b7eef2707cf111f7e72a
Julian Johannesen <julian.johannesen@gmail.com>
2019-10-27 23:28:39 -0400

    Finished phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,7 +1,63 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+class Phrase {
+     constructor(phrase){
+         this.phrase = phrase.toLowerCase();
+         this.letters = this.phrase.split('');
 
- class Phrase {
-     
- }
+         // DOM
+         this.phraseDiv = document.querySelector('#phrase');
+         this.letterLis = this.phraseDiv.firstElementChild.children;
+
+         // Bindings
+         this.addPhraseToDisplay = this.addPhraseToDisplay.bind(this);
+         this.checkLetter = this.checkLetter.bind(this);
+         this.showMatchedLetter = this.showMatchedLetter.bind(this);
+     }
+
+     // Adds letter placeholders to the display when the game starts.  
+    addPhraseToDisplay() {
+        // New fragment to hold placeholder elements
+        const frag = document.createDocumentFragment();
+        // Create placeholder elements and append to frag
+        this.letters.forEach( (letter) => {
+            const li = document.createElement('li');
+            // Test for whether this is a letter or a space
+            const isLetter = /\w/.test(letter);
+            li.className = isLetter ? 'hide letter ' + letter : 'space';
+            li.textContent = isLetter ? letter : ' ';
+            frag.append(li);
+        });
+        // Add placeholders to display
+        this.phraseDiv.firstElementChild.append(frag);
+    }
+
+    // Checks to see if the letter selected by the player matches a 
+    // letter in the phrase.
+    checkLetter(e) {
+        // Create an array of indices by looping through the 
+        // letters of the phrase. Whenever a letter matches the
+        // key clicked by the user, return the letter's index
+        const showThese = [];
+        this.letters.forEach( (letter, index) => {
+            if(e.target.textContent === letter){
+                showThese.push(index);
+            };
+        });
+        // Return the array of indices
+        return showThese;
+    }
+
+    // Reveals the letter(s) on the board that matches the player's 
+    // selection. 
+    showMatchedLetter(e) {
+        // Replace each selected element's 'hide' class with the
+        // 'show' class.
+        const indices = this.checkLetter(e);
+        indices.forEach( (theIndex) => {
+            const theLi = this.letterLis[theIndex];
+            if(theLi){
+                theLi.classList.toggle('hide');
+                theLi.classList.toggle('show');
+            }
+        });
+    }
+}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
+let phrase = new Phrase("Hello, world!");
+phrase.addPhraseToDisplay();
+
+// Add an event listener that listens for the click event on
+// the key, and then checks whether that e.target.value is 
+// equal to one of letters in the word or phrase
+const qwerty = document.querySelector('#qwerty');
+qwerty.addEventListener('click', phrase.showMatchedLetter)
