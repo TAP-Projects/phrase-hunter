94b2c05ef8722c3b3b4fb27155da00549e017a7a
julianjohannesen <github email address>
2019-11-07 22:43:52 -0500

    Done with the meets expectations stuff

--- js/Game.js
+++ js/Game.js
@@ -17,6 +17,12 @@ class Game {
         // The phrase instance
         this.phraseInst = null;
 
+        // Memo for pressed letters
+        this.lettersUsed = '';
+
+        // Hearts images
+        this.hearts = document.querySelectorAll('.heart');
+
         // Bindings
         this.startGame = this.startGame.bind(this);
         this.getRandomPhrase = this.getRandomPhrase.bind(this);
@@ -60,49 +66,86 @@ class Game {
     // In the former case, it calls showMatchedLetter, and in 
     // latter case, it calls removeLife.
     handleInteraction(e) {
-        // Get the key that was just pressed
-        const theLetter = e.target;
-        // Get the text of the key that was just pressed
-        const theLetterText = e.target.textContent;
-        // Store theLetter's current classes
-        let classes = theLetter.className;
-        // If theLetter is in the DOM, then
-        if(this.activePhrase.includes(theLetterText)){  
-            // Add the 'chosen' class
-            classes += ' chosen';
-            theLetter.className = classes;
-            // Call showMatchedLetter
-            this.phraseInst.showMatchedLetter(e);
-
-        } else {
-            // Add the 'wrong' class
-            classes += ' wrong';
-            theLetter.className = classes;
-            // Remove a life
-            this.removeLife();
-        }        
-        
-        // Check to see if the game has been won with checkForWin
-        // and if so, call gameOver()
-        if(this.checkForWin()){
-            this.gameOver()
-        }
-    
+        // Gotta click on a button. 
+        if(e.target.nodeName === 'BUTTON'){
+            // Get the key that was just pressed
+            const theLetter = e.target;
+            // Get the text of the key that was just pressed
+            const theLetterText = e.target.textContent;
+            // Add the letter to the list of used letters
+            if(!this.lettersUsed.includes(theLetterText)){
+                this.lettersUsed += theLetterText;
+            } else {
+                return;
+            }
+            // Store theLetter's current classes
+            let classes = theLetter.className;
+            // If theLetter is in the DOM, then
+            if(this.activePhrase.includes(theLetterText)){  
+                // Add the 'chosen' class
+                classes += ' chosen';
+                theLetter.className = classes;
+                // Call showMatchedLetter
+                this.phraseInst.showMatchedLetter(e);
+                // Check to see if the game has been won 
+                if(this.checkForWin()){
+                    this.gameOver(true);
+                }
+            } else {
+                // Add the 'wrong' class
+                classes += ' wrong';
+                theLetter.className = classes;
+                // Remove a life
+                this.removeLife();
+            }   
+    }     
     }
         
 
     removeLife() {
-        console.log("Remove life fired")
+        if(this.missed < 5){
+            this.hearts[this.hearts.length - 1 - this.missed].src = 'images/lostHeart.png';
+            this.missed++;
+        } else {
+            this.gameOver(false);
+        }
     }
     
+    // Check to see if the player has won or lost
     checkForWin() {
-        console.log("check for win fired");
+        // If the phrase as it appears on the page contains no
+        // hidden elements, then the player has won
+        let theLis = [...document.querySelectorAll('#phrase ul li')];
+        const hiddenLis = theLis.filter(li => li.className.includes('hide'));        
+        if(hiddenLis.length === 0){
+            // Then they've won
+            return true;
+        }
+        // Or they haven't won yet
         return false;
-        
     }
     
-    gameOver() {
-        console.log("Game over fired")
+    // Is the game over? Did the player win or lose?
+    gameOver(won) {
+        // Get the overlay
+        const overlay = document.querySelector('#overlay');
+        // Get the game-over-message
+        const message = document.querySelector('h1#game-over-message');
+        // Replace start screen
+        overlay.style.display = 'flex';
+        if(won){
+            console.log('you won!')
+            // Apply the 'win' style
+            overlay.className = 'win';
+            // Show message
+            message.textContent = 'Congratulations! You won!'
+        }
+        else {
+            // Apply the 'lose' style
+            overlay.className = 'lose';
+            // Show message
+            message.textContent = 'You lost! Better luck next time.'
+        }
 
     }
 }
--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
-let newGame;
+const ul = document.querySelector('#phrase ul');
+const buttons = document.querySelectorAll('#qwerty button');
+const hearts = document.querySelectorAll('img.heart');
 // On button cluck, create a new game instance and start the 
 // game by calling startGame().
 document.querySelector('#btn__reset').addEventListener('click', () => {
+    // Remove the current ul and append a new ul
+    ul.innerHTML = '';
+    // Reset the classes on the key buttons
+    buttons.forEach(button => button.className = 'key');
+    // Reset the hearts
+    hearts.forEach(heart => heart.src = 'images/liveHeart.png');
+    // Create a new game instance
     newGame = new Game();
+    // Start the new game
     newGame.startGame();
 });
 
