d0862d3d0234fe0b128c0cfd208e3e630320e08f
Julian Johannesen <julian.johannesen@gmail.com>
2019-12-03 10:29:21 -0500

    removed an old file

--- js/app.js
+++ js/app.js
@@ -3,10 +3,9 @@
 // - While it isn't explicitly stated as a requirement, the checkLetter method typically just returns true or false based on whether or not the letter is in the phrase.
 
 // - There should be an event listener in app.js that listens for onscreen keyboard events and calls the handleInteraction method. It looks like you are doing this in Game.js in the startGame method instead.
-// - If you look closely when you start a new game you can see that the onscreen keyboard briefly shows the styles on the letters from the previous game. If you reset your game when it ends rather than when it starts you can avoid this behavior.
 
 
-// On button cluck, create a new game instance and start the 
+// On button click, create a new game instance and start the 
 // game by calling startGame().
 document.querySelector('#btn__reset').addEventListener('click', () => {
     
